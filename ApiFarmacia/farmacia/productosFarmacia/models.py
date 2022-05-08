46f183c499d8a0d5953a6a092598016747a69b61
NicolasVidalDuoc <86321476+NicolasVidalDuoc@users.noreply.github.com>
2022-05-08 04:29:56 -0400

    Se agrega ApiFarmacia

---
+++ ApiFarmacia/farmacia/productosFarmacia/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Productos(models.Model):
+    id_producto = models.IntegerField(primary_key=True)
+    nombre_producto = models.CharField(max_length=60)
+    stock_producto = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_producto
